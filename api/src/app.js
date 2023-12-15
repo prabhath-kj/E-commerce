8a04fe0df6e062231cf92f92842ac5835b8b3157
prabhath-kj <view.prabhath.kj@gmail.com>
2023-12-15 18:10:49 +0530

    server initialized

---
+++ api/src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import connectDB from "./config/dbConfig.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Connect to the database
+connectDB();
+
+// Middleware
+app.use(helmet()); // Use Helmet middleware for enhanced security headers
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+  })
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("short"));
+
+// Routes
+app.get("/",(req,res)=>res.json({message:"success"}))
+export default app;
